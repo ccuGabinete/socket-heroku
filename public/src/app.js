64b3f9f8240b74c34026054a3d42ceff98fe0fac
ccuGabinete <relatrioccu@gmail.com>
2019-09-22 15:14:25 -0300

    segundo commit

---
+++ public/src/app.js
@@ -0,0 +1,45 @@
+  
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+const documents = {};
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on("connection", socket => {
+    let previousId;
+    const safeJoin = currentId => {
+      socket.leave(previousId);
+      socket.join(currentId);
+      previousId = currentId;
+    };
+  
+    socket.on("getDoc", docId => {
+      safeJoin(docId);
+      socket.emit("document", documents[docId]);
+    });
+  
+    socket.on("addDoc", doc => {
+      documents[doc.id] = doc;
+      safeJoin(doc.id);
+      io.emit("documents", Object.keys(documents));
+      socket.emit("document", doc);
+    });
+  
+    socket.on("editDoc", doc => {
+      console.log(doc);
+      documents[doc.id] = doc;
+      socket.to(doc.id).emit("document", doc);
+    });
+  
+    io.emit("documents", Object.keys(documents));
+  });
